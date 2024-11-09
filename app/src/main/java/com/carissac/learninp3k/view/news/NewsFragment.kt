6df500d48dab82baa7163f22371e0dc6253bbaec
carissa-chandra <carissa.tjan03@gmail.com>
2024-11-09 13:35:50 +0700

    add search bar and search view

--- app/src/main/java/com/carissac/learninp3k/view/news/NewsFragment.kt
+++ app/src/main/java/com/carissac/learninp3k/view/news/NewsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.carissac.learninp3k.databinding.FragmentNewsBinding
 
@@ -15,6 +16,19 @@ class NewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNewsBinding.inflate(inflater, container, false)
+
+        with(binding) {
+            searchViewNews.setupWithSearchBar(searchBarNews)
+            searchViewNews
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBarNews.setText(searchViewNews.text)
+                    searchViewNews.hide()
+                    Toast.makeText(requireContext(), searchViewNews.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+
         return binding.root
     }
 }
