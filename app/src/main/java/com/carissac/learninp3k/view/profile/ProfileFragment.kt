489ef3b4d537e8ec352bf226b6f4b2ff569ada75
carissa-chandra <carissa.tjan03@gmail.com>
2024-11-14 16:45:38 +0700

    add custom actionBar (toolBar) in SettingActivity

--- app/src/main/java/com/carissac/learninp3k/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/carissac/learninp3k/view/profile/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.carissac.learninp3k.view.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.carissac.learninp3k.databinding.FragmentProfileBinding
+import com.carissac.learninp3k.view.setting.SettingActivity
 
 class ProfileFragment : Fragment() {
     private lateinit var binding : FragmentProfileBinding
@@ -16,5 +18,14 @@ class ProfileFragment : Fragment() {
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivIcSetting.setOnClickListener {
+            val intent = Intent(requireContext(), SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/carissac/learninp3k/view/setting/SettingActivity.kt
@@ -0,0 +1,48 @@
+package com.carissac.learninp3k.view.setting
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import com.carissac.learninp3k.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply{
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(false)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.setting) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = systemBars.top
+                leftMargin = systemBars.left
+                bottomMargin = systemBars.bottom
+                rightMargin = systemBars.right
+            }
+
+            WindowInsetsCompat.CONSUMED
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
+    }
+}
